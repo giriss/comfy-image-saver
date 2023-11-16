518c2c90f354ef92a64d5c21d15c11da85de5e62
Rei Koshka <purrogurama@gmail.com>
2023-11-16 11:58:54 +0300

    Reformat long parameter lines

--- nodes.py
+++ nodes.py
@@ -45,7 +45,13 @@ def get_timestamp(time_format):
     return timestamp
 
 
-def make_pathname(filename, seed, modelname, counter, time_format):
+def make_pathname(
+        filename,
+        seed,
+        modelname,
+        counter,
+        time_format,
+):
     filename = filename.replace("%date", get_timestamp("%Y-%m-%d"))
     filename = filename.replace("%time", get_timestamp(time_format))
     filename = filename.replace("%model", modelname)
@@ -54,8 +60,20 @@ def make_pathname(filename, seed, modelname, counter, time_format):
     return filename
 
 
-def make_filename(filename, seed, modelname, counter, time_format):
-    filename = make_pathname(filename, seed, modelname, counter, time_format)
+def make_filename(
+        filename,
+        seed,
+        modelname,
+        counter,
+        time_format,
+):
+    filename = make_pathname(
+        filename,
+        seed,
+        modelname,
+        counter,
+        time_format,
+    )
 
     return get_timestamp(time_format) if filename == "" else filename
 
@@ -209,10 +227,45 @@ class ImageSaveWithMetadata:
 
     CATEGORY = "ImageSaverTools"
 
-    def save_files(self, images, seed_value, steps, cfg, sampler_name, scheduler, positive, negative, modelname, quality_jpeg_or_webp,
-                   lossless_webp, width, height, counter, filename, path, extension, time_format, prompt=None, extra_pnginfo=None):
-        filename = make_filename(filename, seed_value, modelname, counter, time_format)
-        path = make_pathname(path, seed_value, modelname, counter, time_format)
+    def save_files(
+            self,
+            images,
+            seed_value,
+            steps,
+            cfg,
+            sampler_name,
+            scheduler,
+            positive,
+            negative,
+            modelname,
+            quality_jpeg_or_webp,
+            lossless_webp,
+            width,
+            height,
+            counter,
+            filename,
+            path,
+            extension,
+            time_format,
+            prompt=None,
+            extra_pnginfo=None,
+    ):
+        filename = make_filename(
+            filename,
+            seed_value,
+            modelname,
+            counter,
+            time_format,
+        )
+
+        path = make_pathname(
+            path,
+            seed_value,
+            modelname,
+            counter,
+            time_format,
+        )
+
         ckpt_path = folder_paths.get_full_path("checkpoints", modelname)
         basemodelname = parse_name(modelname)
         modelhash = calculate_sha256(ckpt_path)[:10]
